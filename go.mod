acc0ca6a44de1f38da46717b0f524e4a59476f83
David Möller <davidm@student.chalmers.se>
2020-09-05 17:00:52 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/molleer/kpi-chart-generator
+
+go 1.15
+
+require (
+	github.com/go-echarts/go-echarts v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/go-echarts/go-echarts/charts"
+)
+
+func main() {
+	nameItems := []string{"One", "Two", "Three", "Four", "Five", "Six"}
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.TitleOpts{Title: "Title"})
+	bar.AddXAxis(nameItems).
+		AddYAxis("Hi", []int{1, 2, 3, 4, 5, 6}, charts.BarOpts{Stack: "stack"}).
+		AddYAxis("Hello", []int{-6, -5, -4, -3, -2, -1}, charts.BarOpts{Stack: "stack"})
+	f, _ := os.Create("bar.kpi.html")
+	bar.Render(f)
+}
